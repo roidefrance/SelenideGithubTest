206a323b7dbdbe4c9c4731202159eafeccf1a837
roidefrance <kirmaxmel@gmail.com>
2021-02-23 16:17:28 +0000

    5th commit - added code, updated build.gradle

--- src/test/java/tests/SelenideGithubTest.java
+++ src/test/java/tests/SelenideGithubTest.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
@@ -19,12 +20,15 @@ public class SelenideGithubTest {
     }
     @Test
     void PageTest() {
-    open("https://github.com/selenide/selenide");
-    $$("ul.UnderlineNav-body.list-style-none li").get(4).$("a").click();
+    open("https://github.com");
+    $(byName("q")).val(("selenide")).pressEnter();
+    $(".repo-list a").click();
+    // open("https://github.com/selenide/selenide");
+    $(byLinkText("Wiki")).click();
+    // альтернатива $$("ul.UnderlineNav-body.list-style-none li").get(4).$("a").click();
     $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
     $(byLinkText("SoftAssertions")).click();
     // альтернатива $$("ul.m-0.p-0.list-style-none li").get(14).$("a").click();
-    $(".markdown-body").shouldHave(text("JUnit5"));
-    sleep(1000);
+    $(".markdown-body").shouldHave(text("Using JUnit5"));
     }
 }
